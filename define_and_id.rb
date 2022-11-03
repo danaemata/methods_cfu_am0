42fbeab8642270161878c2e785803fc9eb11303c
Danae Mata <danae.mata1515@gmail.com>
2022-11-03 14:42:55 -0400

    Add answers to questions

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,28 @@
 
 # YOU DO the rest:
 
-
+# This code is using the 'include?' method. It is calling out if the String "Hello World" has the word "Hello in the code."
+# Include looks into the String to find the word "Hello."
+# Since it found the word "Hello," the return value was "True"
 "Hello World".include?("Hello")
 
-
+# This code is using the "end_with?" method. It is calling out to see if the string ends with "Hello."
+# The string ends in the word "World," so the return value is "False."
 "Hello World".end_with?("Hello")
 
-
+# This code is using the "end_with?" method. It is calling out to check if the string ends with "rld."
+# The word "World" does end with "rld." So the return value is "true."
+# If we were looking for the entire word "World," then we would have to specify like in the example above.
 "Hello World".end_with?("rld")
 
-
+# This code is using the "even?" method. THIS IS NOT TO BE MISTAKEN FOR A STRING...IT IS AN INTEGER.
+# It is calling out to see if the integer is an even number.
+# The return value was "true."
 12.even?
 
-
+# This code is using the "next" method. It is calling out to see what integer follows the one that
+# tested.
+# The return value was "19." 
 18.next
 
 
